5bbe7a04f2e689dfde824674b946aeb90d7e30cf
gopal-sakshi <gopal612@gmail.com>
2022-09-27 13:51:10 +0530

    triggers24

--- src/main-handler.js
+++ src/main-handler.js
@@ -18,6 +18,20 @@ async function getUpcomingTriggers(dateObj) {
     // let sql2 = `SELECT * from cron_table23 limit 2`;
     // await init();
     // await pgInterface.execute(sql2);
+
+    /* 
+        HOW IT WORKS 
+        - getUpcomingTriggers() ---> gets called every 30 seconds ---> we used setInterval() in iterator.js
+        - from this cron_table23... 
+            we get list of events
+        - Based on that, we call eventTrigger() endpoint in "games-for-sustainability" repo
+        - inside the eventTrigger() method ---> we send FCM notification & stuff 
+            whether event changed ----->    from one mode to another
+                                    --->    whether we moved to next round
+                                    --->    whether game time is over (or) not
+
+    */
+
 }
 
 async function callEventTriggerHandler(eventId, type) {}
